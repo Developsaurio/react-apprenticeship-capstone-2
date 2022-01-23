184fefbd9ea540c35a23cfab06d818a52112f164
⚡An1k4 ⚡ <rt.anakaren@gmail.com>
2022-01-23 16:55:46 -0600

    main structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Loading from './components/Loading';
+const NASA_URL= 'https://api.nasa.gov/planetary/apod';
+const KEY = 'p6pWJpQEPAfcfezyNmh4Ru5Bt9onXU4Fb42QXAWa';
 
-function App() {
+
+const App = () => {
+  const [data, setData] = useState();
+
+  const url = `${NASA_URL}?api_key=${KEY}`;
+
+  useEffect(() => {      
+    fetch(url).then((res)=> res.json()).then((items) =>  setData(items));    
+  }, []);
+
+  !!data && console.log('DATa', data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     { !!data ?  
+     <>
+      <h2>{!!data && data.title}</h2>
+      <h3>{!!data && data.date}</h3>
+      <h5>{!!data && data.explanation}</h5>
+      {!!data && data.media_type === "image" &&  <img src={data.url} alt={data.title}/>}
+      {!!data && data.media_type === "video" &&  
+                  <iframe 
+                  src={data.url}
+                  title={data.title}
+                  frameBorder="0" 
+                  allow="accelerometer; 
+                          autoplay; 
+                          clipboard-write; 
+                          encrypted-media; 
+                          gyroscope; 
+                          picture-in-picture" 
+                  allowFullScreen
+                  />
+        }
+     </>
+     : <Loading />
+     }
     </div>
   );
 }
